aed9c4f453cbcc9a4ab4a22ad5c9646719753992
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-01-20 13:52:58 -0500

    Scaffold out the pattern forconsent section composition

--- AsthmaHealth/Models/ACMConsentDocument.m
+++ AsthmaHealth/Models/ACMConsentDocument.m
@@ -10,16 +10,35 @@ static NSString *const ACMSignatureIdentifier = @"ACMConsentParticipantConsentSi
     self = [super init];
     if (nil == self) return nil;
 
-    ORKConsentSection *overviewSection = [[ORKConsentSection alloc] initWithType:ORKConsentSectionTypeOverview];
-    overviewSection.summary = NSLocalizedString(@"This is the Summary", nil);
-    overviewSection.content = NSLocalizedString(@"This is the content of the overview section. Things will happen", nil);
-
-    self.sections = @[ overviewSection ];
+    self.sections = ACMConsentDocument.sections;
 
     ORKConsentSignature *signature = [ORKConsentSignature signatureForPersonWithTitle:nil dateFormatString:nil identifier:ACMSignatureIdentifier];
     [self addSignature:signature];
 
     return self;
+}
+
+#pragma mark Static Private Helpers
+
++ (NSArray<ORKConsentSection *> *)sections
+{
+    return @[self.overviewSection];
+}
+
++ (ORKConsentSection *)sectionWithType:(ORKConsentSectionType)type withSummary:(NSString *)summary andContent:(NSString *)content
+{
+    ORKConsentSection *section = [[ORKConsentSection alloc] initWithType:type];
+    section.summary = summary;
+    section.content = content;
+
+    return section;
+}
+
++ (ORKConsentSection *)overviewSection
+{
+    return [self sectionWithType:ORKConsentSectionTypeOverview
+                     withSummary:NSLocalizedString(@"This is the Summary", nil)
+                      andContent:NSLocalizedString(@"This is the content of the overview section. Things will happen", nil)];
 }
 
 @end
