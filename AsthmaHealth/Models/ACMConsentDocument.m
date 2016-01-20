48b10512abf171c2b69b32cf96a199dc2f354799
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-01-20 14:18:53 -0500

    Hardcode a placeholder for the welcome screen

--- AsthmaHealth/Models/ACMConsentDocument.m
+++ AsthmaHealth/Models/ACMConsentDocument.m
@@ -22,7 +22,7 @@ static NSString *const ACMSignatureIdentifier = @"ACMConsentParticipantConsentSi
 
 + (NSArray<ORKConsentSection *> *)sections
 {
-    return @[self.overviewSection];
+    return @[self.welcomeSection];
 }
 
 + (ORKConsentSection *)sectionWithType:(ORKConsentSectionType)type withSummary:(NSString *)summary andContent:(NSString *)content
@@ -34,11 +34,11 @@ static NSString *const ACMSignatureIdentifier = @"ACMConsentParticipantConsentSi
     return section;
 }
 
-+ (ORKConsentSection *)overviewSection
++ (ORKConsentSection *)welcomeSection
 {
     return [self sectionWithType:ORKConsentSectionTypeOverview
-                     withSummary:NSLocalizedString(@"This is the Summary", nil)
-                      andContent:NSLocalizedString(@"This is the content of the overview section. Things will happen", nil)];
+                     withSummary:NSLocalizedString(@"Mount Sinai is doing a research study about whether an asthma app on your iPhone will help you to monitor your asthma. This simple walkthrough will help you to understand the study, the impact it will have on your life, and will allow you to provide consent to participate.", nil)
+                      andContent:NSLocalizedString(@"People with asthma do best if they actively manage their asthma. This app uses your mobile phone to help you do this. If you take daily asthma medicine, the app will help you remember to take it on schedule. There are links to learn more about asthma, how to use your medicine, what triggers your asthma, and how to recognize when your asthma is out of control. The information you put into the app will help you track how well your asthma is controlled and how you are doing compared to other app users with asthma. Although we will look at the data at different points during the study, we will not be looking at your asthma information each day. The app does not replace your usual medical care, so if your asthma is getting worse during the study, please reach out to your health care team", nil)];
 }
 
 @end
