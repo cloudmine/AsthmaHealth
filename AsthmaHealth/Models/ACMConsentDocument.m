e2e60b48645a2e3bb88cdb4ad7aa44ab238e378b
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-01-20 13:40:07 -0500

    Add a review/signature step to the consent task

--- AsthmaHealth/Models/ACMConsentDocument.m
+++ AsthmaHealth/Models/ACMConsentDocument.m
@@ -1,7 +1,10 @@
 #import "ACMConsentDocument.h"
 
+static NSString *const ACMSignatureIdentifier = @"ACMConsentParticipantConsentSignature";
+
 @implementation ACMConsentDocument
 
+// TODO: Ponder, is a subclass actually neccessary?
 - (instancetype)init
 {
     self = [super init];
@@ -12,6 +15,9 @@
     overviewSection.content = NSLocalizedString(@"This is the content of the overview section. Things will happen", nil);
 
     self.sections = @[ overviewSection ];
+
+    ORKConsentSignature *signature = [ORKConsentSignature signatureForPersonWithTitle:nil dateFormatString:nil identifier:ACMSignatureIdentifier];
+    [self addSignature:signature];
 
     return self;
 }
--- AsthmaHealth/Models/ACMConsentTask.m
+++ AsthmaHealth/Models/ACMConsentTask.m
@@ -1,16 +1,23 @@
 #import "ACMConsentTask.h"
 
+static NSString *const ACMReviewConsentIdentifier = @"ACMParticipantConsentReview";
+
 @implementation ACMConsentTask
 
 #pragma mark Factory
 
+// TODO: Ponder, is a subclass actually neccessary?
 + (instancetype)task
 {
     ACMConsentDocument *consentDoc = [ACMConsentDocument new];
 
     ORKVisualConsentStep *consentStep = [[ORKVisualConsentStep alloc] initWithIdentifier:@"Constent Step Id" document:consentDoc];
 
-    ACMConsentTask *consentTask = [[ACMConsentTask alloc] initWithIdentifier:@"Identifier" steps:@[consentStep]];
+    ORKConsentReviewStep *reviewStep = [[ORKConsentReviewStep alloc] initWithIdentifier:ACMReviewConsentIdentifier signature:consentDoc.signatures.firstObject inDocument:consentDoc];
+    reviewStep.text = NSLocalizedString(@"Review ACM Consent", nil);
+    reviewStep.reasonForConsent = NSLocalizedString(@"Consent to Join ACM Study", nil);
+
+    ACMConsentTask *consentTask = [[ACMConsentTask alloc] initWithIdentifier:@"Identifier" steps:@[consentStep, reviewStep]];
 
     return consentTask;
 }
