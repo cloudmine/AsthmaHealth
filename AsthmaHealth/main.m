8e035f1eb9653077ed6b5c65be5bb7d2b45236db
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-01-20 10:42:57 -0500

    Initial project creation

---
+++ AsthmaHealth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AsthmaHealth
+//
+//  Created by Ben DiFrancesco on 1/20/16.
+//  Copyright © 2016 CloudMine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ AsthmaHealthTests/AsthmaHealthTests.m
@@ -0,0 +1,39 @@
+//
+//  AsthmaHealthTests.m
+//  AsthmaHealthTests
+//
+//  Created by Ben DiFrancesco on 1/20/16.
+//  Copyright © 2016 CloudMine. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AsthmaHealthTests : XCTestCase
+
+@end
+
+@implementation AsthmaHealthTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
