4fa1585f6b7fb15db253c423c3b9e2685704f53a
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-01-21 16:36:53 -0500

    Add UI mechanics to the Registration 'Next' button

--- AsthmaHealth/ViewControllers/ACMSignUpViewController.m
+++ AsthmaHealth/ViewControllers/ACMSignUpViewController.m
@@ -2,6 +2,11 @@
 
 @interface ACMSignUpViewController ()
 
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *nextButton;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (nonatomic, readonly) BOOL isValidInput;
+
 @end
 
 @implementation ACMSignUpViewController
@@ -9,6 +14,40 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.nextButton.enabled = self.isValidInput;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleTextChange) name:UITextFieldTextDidChangeNotification object:nil];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+#pragma mark Notifications
+
+- (void)handleTextChange
+{
+    self.nextButton.enabled = self.isValidInput;
+}
+
+#pragma mark Private Helpers
+
+- (BOOL)isValidInput
+{
+    return self.enteredValidEmail && self.enteredValidPassword;
+}
+
+- (BOOL)enteredValidEmail
+{
+    // TODO: real email validation
+    return nil != self.emailTextField.text && self.emailTextField.text.length > 3;
+}
+
+- (BOOL)enteredValidPassword
+{
+    // TODO: real password validation
+    return nil != self.passwordTextField.text && self.passwordTextField.text.length > 5;
 }
 
 @end
