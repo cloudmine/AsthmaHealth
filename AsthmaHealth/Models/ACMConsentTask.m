7c13d460207f9cee87882890a6601a656f7221b8
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-01-21 13:56:17 -0500

    Add a sharing options question to the consent gathering flow

--- AsthmaHealth/Models/ACMConsentTask.m
+++ AsthmaHealth/Models/ACMConsentTask.m
@@ -1,6 +1,9 @@
 #import "ACMConsentTask.h"
 
-static NSString *const ACMReviewConsentIdentifier = @"ACMParticipantConsentReview";
+static NSString *const ACMConsentStepIdentifier     = @"ACMParticipantConsentStep";
+static NSString *const ACMReviewConsentIdentifier   = @"ACMParticipantConsentReview";
+static NSString *const ACMSharingQuestionIdentifier = @"ACMParticipantConsentSharingQuestion";
+static NSString *const ACMConsentTaskIdentifier     = @"ACMParticipantConsentTask";
 
 @implementation ACMConsentTask
 
@@ -11,15 +14,45 @@ static NSString *const ACMReviewConsentIdentifier = @"ACMParticipantConsentRevie
 {
     ACMConsentDocument *consentDoc = [ACMConsentDocument new];
 
-    ORKVisualConsentStep *consentStep = [[ORKVisualConsentStep alloc] initWithIdentifier:@"Constent Step Id" document:consentDoc];
+    ORKVisualConsentStep *consentStep = [[ORKVisualConsentStep alloc] initWithIdentifier:ACMConsentStepIdentifier document:consentDoc];
 
     ORKConsentReviewStep *reviewStep = [[ORKConsentReviewStep alloc] initWithIdentifier:ACMReviewConsentIdentifier signature:consentDoc.signatures.firstObject inDocument:consentDoc];
     reviewStep.text = NSLocalizedString(@"Review ACM Consent", nil);
     reviewStep.reasonForConsent = NSLocalizedString(@"Consent to Join ACM Study", nil);
 
-    ACMConsentTask *consentTask = [[ACMConsentTask alloc] initWithIdentifier:@"Identifier" steps:@[consentStep, reviewStep]];
+    ACMConsentTask *consentTask = [[ACMConsentTask alloc] initWithIdentifier:ACMConsentTaskIdentifier steps:@[consentStep, self.sharingOptionStep, reviewStep]];
 
     return consentTask;
+}
+
+# pragma mark Private Factories
+
++ (ORKQuestionStep *)sharingOptionStep
+{
+    ORKTextChoiceAnswerFormat *format = [[ORKTextChoiceAnswerFormat alloc] initWithStyle:ORKChoiceAnswerStyleSingleChoice textChoices:self.sharingChoices];
+
+    ORKQuestionStep *question = [ORKQuestionStep questionStepWithIdentifier:ACMSharingQuestionIdentifier
+                                                                      title:NSLocalizedString(@"Sharing Option", nil)
+                                                                       text:NSLocalizedString(@"Mount Sinai will receive your study data from your participation in this study.\n\nSharing your coded study data more broadly (without information such as your name) may benefit this and future research.", nil)
+                                                                     answer:format];
+    question.optional = NO;
+    
+    return question;
+}
+
++ (NSArray<ORKTextChoice *> *)sharingChoices
+{
+    ORKTextChoice *choice1 = [[ORKTextChoice alloc] initWithText:NSLocalizedString(@"Share my data with Mount Sinai and qualified researchers worldwide", nil)
+                                                      detailText:nil
+                                                           value:@"ACMSharingOptionBroad"
+                                                       exclusive:YES];
+
+    ORKTextChoice *choice2 = [[ORKTextChoice alloc] initWithText:NSLocalizedString(@"Only share my data with Mount Sinai and its partners", nil)
+                                                      detailText:nil
+                                                           value:@"ACMSharingOptionNarrow"
+                                                       exclusive:YES];
+
+    return @[choice1, choice2];
 }
 
 @end
