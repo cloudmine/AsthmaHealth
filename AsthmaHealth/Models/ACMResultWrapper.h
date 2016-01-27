ff330b1cf22cd7a590d5846a2185569025157cf8
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-01-27 14:14:08 -0500

    Working implementation of cm_save: to save survey data to CloudMine

---
+++ AsthmaHealth/Models/ACMResultWrapper.h
@@ -0,0 +1,13 @@
+//
+//  ACMResultWrapper.h
+//  AsthmaHealth
+//
+//  Created by Ben DiFrancesco on 1/27/16.
+//  Copyright © 2016 CloudMine. All rights reserved.
+//
+
+#import <CloudMine/CloudMine.h>
+
+@interface ACMResultWrapper : CMObject
+
+@end
---
+++ AsthmaHealth/Models/ACMResultWrapper.m
@@ -0,0 +1,13 @@
+//
+//  ACMResultWrapper.m
+//  AsthmaHealth
+//
+//  Created by Ben DiFrancesco on 1/27/16.
+//  Copyright © 2016 CloudMine. All rights reserved.
+//
+
+#import "ACMResultWrapper.h"
+
+@implementation ACMResultWrapper
+
+@end
