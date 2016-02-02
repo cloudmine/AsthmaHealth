4e8d04cc496366a5e58080817ab324c04e03e516
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-02-02 17:03:54 -0500

    Basic UI mechanics for login screen

--- AsthmaHealth/ViewControllers/ACMLoginViewController.m
+++ AsthmaHealth/ViewControllers/ACMLoginViewController.m
@@ -12,12 +12,46 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.doneButton.enabled = NO;
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleTextChange) name:UITextFieldTextDidChangeNotification object:nil];
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 #pragma mark Target-Action
 - (IBAction)doneButtonDidPress:(UIBarButtonItem *)sender
 {
     NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+#pragma mark Notifications
+
+- (void)handleTextChange
+{
+    self.doneButton.enabled = self.isValidInput;
+}
+
+#pragma mark Private Helpers
+
+- (BOOL)isValidInput
+{
+    return self.enteredValidEmail && self.enteredValidPassword;
+}
+
+- (BOOL)enteredValidEmail
+{
+    // TODO: real email validation
+    return nil != self.emailTextField.text && self.emailTextField.text.length > 3;
+}
+
+- (BOOL)enteredValidPassword
+{
+    // TODO: real password validation
+    return nil != self.passwordTextField.text && self.passwordTextField.text.length > 5;
 }
 
 @end
