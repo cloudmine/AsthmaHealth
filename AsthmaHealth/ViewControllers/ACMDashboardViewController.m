46c0b319c73a6e23c2547589719fc0fa8406f6cf
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-02-03 16:18:45 -0500

    Add and connect a Dashboard view controller

---
+++ AsthmaHealth/ViewControllers/ACMDashboardViewController.m
@@ -0,0 +1,15 @@
+#import "ACMDashboardViewController.h"
+
+@interface ACMDashboardViewController ()
+
+@end
+
+@implementation ACMDashboardViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+@end
