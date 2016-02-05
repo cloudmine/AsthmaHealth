d7b797efc0d772c3b889cb097895813203b9578f
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-02-05 12:43:33 -0500

    Display a list of surveys and their counts in the Dashboard Survey details screen

--- AsthmaHealth/ViewControllers/ACMDashSurveysViewController.m
+++ AsthmaHealth/ViewControllers/ACMDashSurveysViewController.m
@@ -1,7 +1,7 @@
 #import "ACMDashSurveysViewController.h"
 
 @interface ACMDashSurveysViewController ()
-
+@property (nonatomic, nonnull) NSDictionary<NSString *, NSNumber *> *surveyCounts;
 @end
 
 @implementation ACMDashSurveysViewController
@@ -9,7 +9,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"%@", self.surveyResults);
+    self.surveyCounts = [ACMDashSurveysViewController surveyCountsForResults:self.surveyResults];
 }
 
 #pragma mark UITableViewDataSource
@@ -21,13 +21,51 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return self.surveyCounts.allKeys.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SurveyDetailCell" forIndexPath:indexPath];
+
+    NSArray *sortedKeys = [self.surveyCounts.allKeys sortedArrayUsingSelector:@selector(compare:)];
+    cell.textLabel.text = sortedKeys[indexPath.row];
+    cell.detailTextLabel.text = self.surveyCounts[sortedKeys[indexPath.row]].stringValue;
+
     return cell;
+}
+
+#pragma mark Getters-Setters
+- (NSDictionary<NSString *,NSNumber *> *)surveyCounts
+{
+    if (nil == _surveyCounts) {
+        _surveyCounts = @{ };
+    }
+
+    return _surveyCounts;
+}
+
+#pragma mark Private
++ (NSDictionary<NSString *, NSNumber *> *_Nonnull)surveyCountsForResults:(NSArray<ORKTaskResult *> *_Nullable)results
+{
+    if (nil == results) {
+        return @{ };
+    }
+
+    NSMutableDictionary *mutableCounts = [NSMutableDictionary new];
+
+    for (ORKTaskResult *aResult in results) {
+        NSNumber *currentCount = [mutableCounts objectForKey:aResult.identifier];
+
+        if (nil == currentCount) {
+            [mutableCounts setObject:@1 forKey:aResult.identifier];
+        } else {
+            NSNumber *incrementedCount = [NSNumber numberWithInteger:currentCount.integerValue + 1];
+            [mutableCounts setObject:incrementedCount forKey:aResult.identifier];
+        }
+    }
+
+    return [mutableCounts copy];
 }
 
 @end
