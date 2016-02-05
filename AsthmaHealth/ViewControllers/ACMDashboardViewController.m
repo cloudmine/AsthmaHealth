5f36e176378d56f223267d949724114b16ef2a49
Ben DiFrancesco <ben.difrancesco@gmail.com>
2016-02-05 11:07:42 -0500

    Perform some filtering on the fetched results inside the Dashboard controller

--- AsthmaHealth/ViewControllers/ACMDashboardViewController.m
+++ AsthmaHealth/ViewControllers/ACMDashboardViewController.m
@@ -5,7 +5,8 @@
 #import "ORKResult+CloudMine.h"
 
 @interface ACMDashboardViewController ()
-
+@property (nonatomic, nullable) ORKTaskResult *consentResult;
+@property (nonatomic, nullable) NSArray <ORKTaskResult *> *surveyResults;
 @end
 
 @implementation ACMDashboardViewController
@@ -13,11 +14,40 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"%s", __PRETTY_FUNCTION__);
 
     [ORKTaskResult cm_fetchUserResultsWithCompletion:^(NSArray * _Nullable results, NSError * _Nullable error) {
-        NSLog(@"Results: %@", results);
+        if (nil == results) {
+            NSLog(@"%@", error.localizedDescription);
+            return;
+        }
+
+        ORKResult *possibleConsent = (ORKTaskResult *)[ACMDashboardViewController resultsWithIdentifier:@"ACMParticipantConsentTask" fromResults:results].firstObject;
+
+        NSMutableArray *mutableResults = [results mutableCopy];
+        [mutableResults removeObject:self.consentResult];
+        self.surveyResults = [mutableResults copy];
     }];
+}
+
+#pragma mark Private
+
++ (NSArray<ORKResult *> *_Nonnull)resultsWithIdentifier:(NSString *_Nonnull)identifier fromResults:(NSArray<ORKResult *> *_Nullable)fullResults
+{
+    if (nil == fullResults) {
+        return @[];
+    }
+
+    NSMutableArray *mutableFilteredResults = [NSMutableArray new];
+
+    for (ORKResult *aResult in fullResults) {
+        if (![aResult.identifier isEqualToString:identifier]) {
+            continue;
+        }
+
+        [mutableFilteredResults addObject:aResult];
+    }
+
+    return [mutableFilteredResults copy];
 }
 
 @end
